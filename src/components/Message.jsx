e588947158188c2ec58395ff0340817376135308
diegomillandev <millan@iMac-Pro.local>
2024-03-06 19:04:31 -0500

    add useReg, useMemo componente message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,34 @@
+import { useRef, useMemo } from "react";
+import { brands, plans } from "../constants";
+
 export const Message = ({ quote, data }) => {
     const { year, plan, brand } = data;
-    const brands = { 1: "American", 2: "European", 3: "Asian" };
-    const plans = { 1: "Basic", 2: "Complete", 3: "Full" };
+    const yearSeleted = useRef(year);
+    const [{ name: brandSelected }] = useMemo(
+        () => brands.filter((item) => item.id === Number(brand)),
+        [quote]
+    );
+    const [{ name: planSelected }] = useMemo(
+        () => plans.filter((item) => item.id === Number(plan)),
+        [quote]
+    );
+
     return (
         <div className="bg-green-300 py-3 border border-green-800 rounded flex justify-center">
-            <div className="text-green-950 w-1/4 text-left">
+            <div className="text-green-950 text-left">
                 <p className="font-bold">
-                    Brand: <span className="font-normal">{brands[brand]}</span>
+                    Brand: <span className="font-normal">{brandSelected}</span>
                 </p>
                 <p className="font-bold">
-                    Year: <span className="font-normal">{year}</span>
+                    Year:{" "}
+                    <span className="font-normal">{yearSeleted.current}</span>
                 </p>
                 <p className="font-bold">
-                    Plan: <span className="font-normal">{plans[plan]}</span>
+                    Plan: <span className="font-normal">{planSelected}</span>
                 </p>
                 <p className="font-bold">
-                    The total: <span className="font-normal">${quote}</span>
+                    The total:{" "}
+                    <span className="font-normal">${quote?.toFixed(2)}</span>
                 </p>
             </div>
         </div>
